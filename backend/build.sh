54733dd4455c148c549cc49de4a0b2d372e83ae5
prashanthCannyfore <prashanth.v@cannyfore.com>
2025-10-06 17:32:21 +0530

    build

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Install dependencies
 pip install -r requirements.txt
-# Force Matplotlib to build its cache now, not during server startup
 python -c "import matplotlib.pyplot as plt; plt.figure(); plt.savefig('/tmp/dummy.png')"
