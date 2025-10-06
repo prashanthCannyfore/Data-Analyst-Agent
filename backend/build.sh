6cca9dc32f338f1dc14bd1c379c14d64519edd5a
prashanthCannyfore <prashanth.v@cannyfore.com>
2025-10-06 23:31:26 +0530

    Attempt 6  small 'all-MiniLM-L6-v1'

--- backend/build.sh
+++ backend/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 pip install -r requirements.txt
 python -c "import matplotlib.pyplot as plt; plt.figure(); plt.savefig('/tmp/dummy.png')"
+
+# 3. Save the correct start command to a separate script
+echo 'uvicorn main:app --host 0.0.0.0 --port $PORT' > start.sh
+chmod +x start.sh
